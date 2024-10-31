bf789166257ceadc4d7c39f3ed5f2a9d57c9b388
Liam Hanrahan <60296487+outercloudstudio@users.noreply.github.com>
2024-10-31 18:48:09 -0500

    Update build.ts to use appimage

--- src/build.ts
+++ src/build.ts
@@ -99,6 +99,9 @@ export async function buildProject(
       join(artifactsPath, `bundle/macos/${app.name}.app`),
       join(artifactsPath, `bundle/macos/${app.name}.app.tar.gz`),
       join(artifactsPath, `bundle/macos/${app.name}.app.tar.gz.sig`),
+      join(artifactsPath, `bundle/appimage/${app.name}.AppImage`),
+      join(artifactsPath, `bundle/appimage/${app.name}.AppImage.tar.gz`),
+      join(artifactsPath, `bundle/appimage/${app.name}.AppImage.tar.gz.sig`),
     ].map((path) => ({ path, arch }));
   } else if (targetInfo.platform === 'windows') {
     if (arch.startsWith('i')) {
